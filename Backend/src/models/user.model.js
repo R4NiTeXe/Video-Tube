5c86fe4c139f9d2854b4bace5d75ed0703f1b502
ranit_exe <ranitnaskar09032007@gmail.com>
2026-07-03 20:46:15 +0530

    feat: create user and video schemas

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,191 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+      lowercase: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    mobile: {
+      type: String,
+      trim: true,
+      unique: true,
+      sparse: true,
+    },
+    isMobileVerified: {
+      type: Boolean,
+      default: false,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    avatarPublicId: {
+      type: String,
+    },
+    coverImage: {
+      type: String,
+    },
+    coverImagePublicId: {
+      type: String,
+    },
+    channelBanner: {
+      type: String,
+    },
+    channelBannerPublicId: {
+      type: String,
+    },
+    bio: {
+      type: String,
+      default: "",
+      maxlength: 500,
+    },
+    socialLinks: {
+      youtube: { type: String, default: "" },
+      twitter: { type: String, default: "" },
+      instagram: { type: String, default: "" },
+      github: { type: String, default: "" },
+      website: { type: String, default: "" },
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    emailVerificationToken: {
+      type: String,
+    },
+    emailVerificationExpires: {
+      type: Date,
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    notificationPrefs: {
+      likes: { type: Boolean, default: true },
+      comments: { type: Boolean, default: true },
+      replies: { type: Boolean, default: true },
+      subscriptions: { type: Boolean, default: true },
+      mentions: { type: Boolean, default: true },
+    },
+    isPrivate: {
+      type: Boolean,
+      default: false,
+    },
+    blockedUsers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    mutedUsers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    watchLater: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    searchHistory: [
+      {
+        type: String,
+      },
+    ],
+    passwordResetToken: {
+      type: String,
+    },
+    passwordResetExpires: {
+      type: Date,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+    socialAccounts: {
+      type: Map,
+      of: String,
+      default: {},
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// using the pre function to encrypt the password
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+// this method is used to compare the password
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
---
+++ Backend/src/models/video.model.js
@@ -0,0 +1,84 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+const videoSchema = new Schema(
+  {
+    videoFile:{
+        type: String,   
+        required:true,
+    },
+    thumbnail:{
+        type:String,
+        required:true,
+    },
+    title:{
+        type:String,
+        required:true,
+        index:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    duration:{
+        type:Number,
+        required:true,
+    },
+    views:{
+        type:Number,
+        default:0,
+    },
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    },
+    isPublished:{
+        type:Boolean,
+        default:true,
+    },
+    tags:{
+        type:[String],
+        default:[],
+    },
+    category:{
+        type:String,
+        default:"General",
+    },
+    chapters:[{
+        title:{ type:String },
+        startTime:{ type:Number },
+    }],
+    scheduledAt:{
+        type:Date,
+    },
+    likesCount:{
+        type:Number,
+        default:0,
+    },
+    commentsCount:{
+        type:Number,
+        default:0,
+    },
+    isShort:{
+        type:Boolean,
+        default:false,
+    },
+    isPaid:{
+        type:Boolean,
+        default:false,
+    },
+    price:{
+        type:Number,
+        default:0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// adding the plugin for pagination
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
